9ae97666baf1c0edec7c6efd397a4d0937fd7824
Rylyegh <bartlomiej.sojka@student.put.poznan.pl>
2022-04-15 17:08:49 +0200

    fbf

--- Project1.py
+++ Project1.py
@@ -30,26 +30,35 @@ if code == "RZ":
 else:
     Bsig = Bit_rate*0.5
 
-Bsystem = Bsig*1.1
-print("Bandwidth system = ",Bsystem)
-Btx = Bsystem*1.9
-print("Bandwidth transmitter and receiver = ",Btx)
-delta_ttx = 0.35/(Btx*10**6)
-delta_tch = 0.35/(Bsystem*10**6)
-delta_tf = math.sqrt((delta_tch)**2-2*(delta_ttx)**2)
-print("delta_f in ps = ",delta_tf/10**-12)
 
-D_min = (dispersion_slope/4)*(min_op_wavelength-(center_frequency**4/min_op_wavelength**3))+(dispersion_slope/2)*RMS
-D_max = (dispersion_slope/4)*(max_op_wavelength-(center_frequency**4/max_op_wavelength**3))+(dispersion_slope/2)*RMS
 
-L_bandwidth_min = (delta_tf/10**-12)/(abs(D_min)*RMS)
-L_bandwidth_max = (delta_tf/10**-12)/(abs(D_max)*RMS)
+Bsystem = Bsig * 1.1
 
-print("Minimum Bandwidth length: ",L_bandwidth_min)
-print("Maximum Bandwidth length: ",L_bandwidth_max)
+Btx = Bsystem * 1.9
 
-L_power_min = (min_Ptx_value-min_sens_Prx-3-path_penalty-2*0.5)/(attenuation_fiber)
-L_power_max = (max_Ptx_value-min_sens_Prx-3-path_penalty-2*0.5)/(attenuation_fiber)
+delta_ttx = 0.35 / (Btx * 10 ** 6)
+delta_tch = 0.35 / (Bsystem * 10 ** 6)
+delta_tf = math.sqrt((delta_tch) ** 2 - 2 * (delta_ttx) ** 2)
+print("delta_f in ps = ", delta_tf / 10 ** -12)
+D_min = (dispersion_slope / 4) * (min_op_wavelength - (center_frequency ** 4 / min_op_wavelength ** 3)) + (
+            dispersion_slope / 2) * RMS
+D_max = (dispersion_slope / 4) * (max_op_wavelength - (center_frequency ** 4 / max_op_wavelength ** 3)) + (
+            dispersion_slope / 2) * RMS
+L_bandwidth_min = (delta_tf / 10 ** -12) / (abs(D_min) * RMS)
+L_bandwidth_max = (delta_tf / 10 ** -12) / (abs(D_max) * RMS)
 
-print("Minimum Power length: ",L_power_min)
-print("Maximum Power length: ",L_power_max)
+
+
+
+
+L_power_min = (min_Ptx_value - min_sens_Prx - 3 - path_penalty - 2 * 0.5) / (attenuation_fiber)
+L_power_max = (max_Ptx_value - min_sens_Prx - 3 - path_penalty - 2 * 0.5) / (attenuation_fiber)
+
+
+
+print("Bandwidth system[MHz] = ", Bsystem)
+print("Bandwidth transmitter and receiver[MHz] = ", Btx)
+print("Minimum Power length[km]: ", L_power_min)
+print("Maximum Power length[km]: ", L_power_max)
+print("Minimum Bandwidth length[km]: ", L_bandwidth_min)
+print("Maximum Bandwidth length[km]: ", L_bandwidth_max)
